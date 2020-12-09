80b9247da15c86132f59d273f94b56610ca75ed6
David García Garzón <david.garcia@somenergia.coop>
2020-12-09 12:58:03 +0100

    indentation

--- src/App.js
+++ src/App.js
@@ -212,7 +212,7 @@ function App() {
                   <Uri options={filterOptions} />
                 </Paper>
 
-              <Paper className={classes.paperTabs}>
+                <Paper className={classes.paperTabs}>
                   <Tabs
                     value={format}
                     onChange={handleChange}
